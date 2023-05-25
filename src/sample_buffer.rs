b80eb42dbc6b349b0f1e4c4fa2bd311d6a8ec710
Jacob Shaffer <1503954+Phyllostachys@users.noreply.github.com>
2023-05-25 16:46:07 -0400

    add `CMSampleBufferCreate` and `CMSampleBufferGetSampleAttachmentsArray`

--- src/sample_buffer.rs
+++ src/sample_buffer.rs
@@ -1,17 +1,18 @@
-use crate::libc::c_void;
-use crate::core_foundation_sys::base::{OSStatus, Boolean, CFAllocatorRef, CFTypeID, CFTypeRef};
+use crate::core_foundation_sys::array::CFArrayRef;
+use crate::core_foundation_sys::base::{Boolean, CFAllocatorRef, CFTypeID, CFTypeRef, OSStatus};
 use crate::core_foundation_sys::dictionary::CFDictionaryRef;
 use crate::core_foundation_sys::string::CFStringRef;
+use crate::libc::{c_void, size_t};
 
-use crate::base::CMItemIndex;
+use crate::base::{CMItemCount, CMItemIndex};
+use crate::block_buffer::CMBlockBufferRef;
+use crate::format_description::CMFormatDescriptionRef;
 use crate::sync::CMClockRef;
 use crate::time::CMTime;
 
-
 pub type CMSampleBufferRef = *mut c_void;
-pub type CMSampleBufferMakeDataReadyCallback = extern "C" fn(sbuf: CMSampleBufferRef, 
-                                                             makeDataReadyRefcon: *mut c_void) -> OSStatus;
-
+pub type CMSampleBufferMakeDataReadyCallback =
+    extern "C" fn(sbuf: CMSampleBufferRef, makeDataReadyRefcon: *mut c_void) -> OSStatus;
 
 #[repr(C)]
 #[derive(Debug, Copy, Clone)]
@@ -21,9 +22,28 @@ pub struct CMSampleTimingInfo {
     pub decode_time_stamp: CMTime,
 }
 
-
 extern "C" {
-    pub fn CMSampleBufferGetSampleTimingInfo(sbuf: CMSampleBufferRef,
-                                             sampleIndex: CMItemIndex,
-                                             timingInfoOut: *mut CMSampleTimingInfo) -> OSStatus;
+    pub fn CMSampleBufferCreate(
+        allocator: CFAllocatorRef,
+        dataBuffer: CMBlockBufferRef,
+        dataReady: bool,
+        makeDataReadyCallback: CMSampleBufferMakeDataReadyCallback,
+        makeDataReadyRefcon: *mut c_void,
+        formatDescription: CMFormatDescriptionRef,
+        numSamples: CMItemCount,
+        numSampleTimingEntries: CMItemCount,
+        sampleTimingArray: *const CMSampleTimingInfo,
+        numSampleSizeEntries: CMItemCount,
+        sampleSizeArray: *const size_t,
+        sampleBufferOut: *mut CMSampleBufferRef,
+    ) -> OSStatus;
+    pub fn CMSampleBufferGetSampleAttachmentsArray(
+        sbuf: CMSampleBufferRef,
+        createIfNecessary: bool,
+    ) -> CFArrayRef;
+    pub fn CMSampleBufferGetSampleTimingInfo(
+        sbuf: CMSampleBufferRef,
+        sampleIndex: CMItemIndex,
+        timingInfoOut: *mut CMSampleTimingInfo,
+    ) -> OSStatus;
 }
